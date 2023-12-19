7c6ac393a954d59dbc1012042cea7a26816f34b0
elisabichiappini <elisabichiappini@gmail.com>
2023-12-20 00:05:24 +0100

    feat: creating header

--- src/main.js
+++ src/main.js
@@ -3,9 +3,29 @@ import '../src/assets/styles/general.scss';
 import App from './App.vue';
 
 
+//fontawesom
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { faFacebookF } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
+import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
+/* add icons to the library */
+library.add(faUserSecret, faFacebookF, faTwitter, faInstagram, faYoutube, faCartShopping, faChevronDown, faMagnifyingGlass);
+
 // Import all of Bootstrap's JS
 import * as bootstrap from 'bootstrap';
 // or, specify which plugins you need:
 import { Tooltip, Toast, Popover } from 'bootstrap';
 
-createApp(App).mount('#app');
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
