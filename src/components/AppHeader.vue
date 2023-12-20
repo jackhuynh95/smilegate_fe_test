dec0339e1bb22bd6d2e6fd6d3b11b185adddc077
elisabichiappini <elisabichiappini@gmail.com>
2023-12-20 13:48:42 +0100

    feat: layout header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,110 +1,19 @@
 <script>
+import AppNavbarTop from './AppNavbarTop.vue';
+import AppNavbarBottom from './AppNavbarBottom.vue';
 export default {
     name: 'HeaderApp',
+    components: {
+        AppNavbarTop,
+        AppNavbarBottom,
+    }
 }
 </script>
 <template>
     <header>
-        <div id="top-header" class="container py-2 d-flex align-items-center justify-content-between">
-            <div class="ec-socials d-flex gap-3">
-                <font-awesome-icon icon="fa-brands fa-facebook-f" />
-                <font-awesome-icon icon="fa-brands fa-twitter" />
-                <font-awesome-icon icon="fa-brands fa-instagram" />
-                <font-awesome-icon icon="fa-brands fa-youtube" />
-            </div>
-            <div class="ec-shop-list">
-                <div class="btn-group" role="group" aria-label="Button group with nested dropdown">
-                    <button type="button" class="btn">Shopping Cart</button>
-                    <button type="button" class="btn">
-                        My Account
-                        <font-awesome-icon icon="fa-solid fa-chevron-down" />
-                    </button>
-
-                    <div class="btn-group" role="group">
-                        <button type="button" class="btn" data-bs-toggle="dropdown">
-                            <font-awesome-icon icon="fa-solid fa-cart-shopping" />
-                            Cart
-                            <font-awesome-icon icon="fa-solid fa-chevron-down" />
-                        </button>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-                            <li><a class="dropdown-item" href="#">Dropdown link</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div id="bottom-header" class="container d-flex align-items-center justify-content-between">
-            <div>logo</div>
-            <nav class="navbar navbar-expand-lg bg-body-tertiary">
-                <div class="container-fluid">
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse gap-2" id="navbarNavDropdown">
-                        <ul class="navbar-nav">
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Features</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Dropdown link
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li><a class="dropdown-item" href="#">Action</a></li>
-                                    <li><a class="dropdown-item" href="#">Another action</a></li>
-                                    <li><a class="dropdown-item" href="#">Something else here</a></li>
-                                </ul>
-                            </li>
-                            <button type="button" class="btn btn-primary ec-btn">
-                                Primary
-                            </button>
-                        </ul>
-                        <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
-                    </div>
-                </div>
-            </nav>
-        </div>
+        <AppNavbarTop></AppNavbarTop>
+        <hr>
+        <AppNavbarBottom></AppNavbarBottom>
     </header>
 </template>
 <style lang="scss" scoped>
@@ -112,16 +21,8 @@ export default {
 header {
     height: 160px;
 
-    #top-header {
-        height: 35%;
-    }
-    #bottom-header {
-        height: 100%;
-        .ec-btn {
-            border-radius:20px;
-        }
-    }
+   hr {
+    margin:0 0 1rem 0;
+   }
 }
-
-
 </style>
---
+++ src/components/AppNavbarBottom.vue
@@ -0,0 +1,113 @@
+<script>
+export default {
+    name: 'NavBarBottomComponent',
+}
+</script>
+
+<template>
+    <div id="bottom-header" class="container d-flex align-items-center justify-content-between">
+        <div>
+            <img src="../../public/classic_shop_logo2x.png" alt="">
+        </div>
+        <nav>
+            <ul class="nav nav-pills">
+                <li class="nav-item dropdown">
+                    <a class="nav-link " data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                        Home
+                        <font-awesome-icon icon="fa-solid fa-chevron-down" />
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                        Shop
+                        <font-awesome-icon icon="fa-solid fa-chevron-down" />
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                        Products
+                        <font-awesome-icon icon="fa-solid fa-chevron-down" />
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                        Categories
+                        <font-awesome-icon icon="fa-solid fa-chevron-down" />
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">News</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link " data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">
+                        Elements
+                        <font-awesome-icon icon="fa-solid fa-chevron-down" />
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Separated link</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item text-uppercase">
+                    <a class="nav-link ec-color-primary rounded-pill px-4" aria-current="page" href="#">Shop now!</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">
+                        <font-awesome-icon icon="fa-solid fa-magnifying-glass" />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+@use '../../src/assets/styles/partials/variables' as *;
+.nav-link {
+    color: $text;
+
+    &:hover {
+        color: $color-primary;
+    }
+}
+.ec-color-primary {
+    background-color: $color-primary;
+    color: $white;
+    &:hover {
+        background-color: $color-secondary;
+        color: $white;
+    }
+}
+</style>
