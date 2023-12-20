ed06c42cc8004a04f812916f81b7ddc66fc9ed60
elisabichiappini <elisabichiappini@gmail.com>
2023-12-20 23:24:35 +0100

    feat: scaffolding section cards top sell

--- src/components/AppCardsTopSell.vue
+++ src/components/AppCardsTopSell.vue
@@ -5,9 +5,69 @@ export default {
 </script>
 
 <template>
-
+<section>
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <h6 class="text-uppercase">Featured</h6>
+                <ul class="d-flex justify-content-between px-0">
+                    <li class="d-flex flex-column">
+                        <span>Black Jacket <strong>$215</strong></span>
+                        <span>stelle</span>
+                        <span>price</span>
+                    </li>
+                    <li>
+                        <img class="img-top-sell" src="../../black_leather_jacket-120x156.jpg" alt="model1">
+                    </li>
+                </ul>
+                <hr>
+                <ul class="d-flex justify-content-between px-0">
+                    <li class="d-flex flex-column">
+                        <span>Black Jacket $215</span>
+                        <span>stelle</span>
+                        <span>price</span>
+                    </li>
+                    <li>
+                        <img class="img-top-sell" src="../../black_leather_jacket-120x156.jpg" alt="model1">
+                    </li>
+                </ul>
+                <hr>
+                <ul class="d-flex justify-content-between px-0">
+                    <li class="d-flex flex-column">
+                        <span>Black Jacket $215</span>
+                        <span>stelle</span>
+                        <span>price</span>
+                    </li>
+                    <li>
+                        <img class="img-top-sell" src="../../black_leather_jacket-120x156.jpg" alt="model1">
+                    </li>
+                </ul>
+                <hr>
+                <ul class="d-flex justify-content-between px-0">
+                    <li class="d-flex flex-column">
+                        <span>Black Jacket $215</span>
+                        <span>stelle</span>
+                        <span>price</span>
+                    </li>
+                    <li>
+                        <img class="img-top-sell" src="../../black_leather_jacket-120x156.jpg" alt="model1">
+                    </li>
+                </ul>
+                <hr>
+            </div>
+        </div>
+    </div>
+</section>
 </template>
 
 <style lang="scss" scoped>
-
+ul {
+    li {
+        list-style-type: none;
+    }
+    .img-top-sell {
+        width: 50px;
+        height: auto;
+    }
+}
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -7,6 +7,7 @@ import AppCardsBestSell from './AppCardsBestSell.vue';
 import AppOffertsBlock from './AppOffertsBlock.vue';
 import AppBlockFeedback from './AppBlockFeedback.vue';
 import AppPostsBlog from './AppPostsBlog.vue';
+import AppCardsTopSell from './AppCardsTopSell.vue';
 export default {
     name: 'Main',
     components: {
@@ -18,6 +19,7 @@ export default {
         AppOffertsBlock,
         AppBlockFeedback,
         AppPostsBlog,
+        AppCardsTopSell,
     }
 };
 </script>
@@ -49,6 +51,7 @@ export default {
         <span>The latest Classic Shop news</span>
     </div>
     <AppPostsBlog></AppPostsBlog>
+    <AppCardsTopSell></AppCardsTopSell>
 </template>
 
 <style lang="scss" scoped>
