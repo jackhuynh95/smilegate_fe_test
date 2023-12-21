de5158af41b26b992c6bd2118b74ff8732ba7ff1
elisabichiappini <elisabichiappini@gmail.com>
2023-12-21 14:10:30 +0100

    refactor: section card featured

--- src/components/AppCardsFeatured.vue
+++ src/components/AppCardsFeatured.vue
@@ -5,55 +5,52 @@ export default {
 </script>
 
 <template>
-    <div class="container d-flex justify-content-center mb-5">
+    <section class="container d-flex justify-content-center mb-5">
         <div class="ec-card-featured">
             <img src="../../black_elegant_leather_jacket-400x520.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h6>Black Leather Jacket</h6>
+                <strong>Black Leather Jacket</strong>
                 <p>Men, Jackets, Jeans</p>
-                <span>$235 $200</span>
+                <p class="price"><span class="text-decoration-line-through">$235</span> $200</p>
             </div>
         </div>
         <div class="ec-card-featured">
             <img src="../../black_leather_suit-400x520.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h6>Black Leather Suit</h6>
+                <strong>Black Leather Suit</strong>
                 <p>Men, Jackets</p>
-                <span>$176</span>
+                <p class="price">$176</p>
             </div>
         </div>
         <div class="ec-card-featured">
             <img src="../../blue_jacket_and_white_stripe_tee-400x520.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h6>Blue Jacket & Stripe Tee</h6>
+                <strong>Blue Jacket & Stripe Tee</strong>
                 <p>Men, Jackets, Suits</p>
-                <span>$580</span>
-            </div>
-        </div>
-        <div class="ec-card-featured">
-            <img src="../../black_elegant_leather_jacket-400x520.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-                <h6>Modern Black Leather Suit</h6>
-                <p>Men, Jackets</p>
-                <span>$96</span>
+                <p class="price">$580</p>
             </div>
         </div>
         <div class="ec-card-featured">
             <img src="../../modern_black_leather_suit-400x520.jpg" class="card-img-top" alt="...">
             <div class="card-body">
-                <h6>Black Leather Jacket</h6>
+                <strong>Black Leather Jacket</strong>
                 <p>Men, Jackets, Jeans</p>
-                <span>$235 $200</span>
+                <p class="price">$96</p>
             </div>
         </div>
-    </div>
+    </section>
         
 </template>
 
 <style lang="scss" scoped>
-
+@use '../assets/styles/partials/variables' as *;
  .ec-card-featured {
-    width: calc(100% / 5);
- }
-
+    width: calc(100% / 4);
+    .card-body {
+        margin-top: 36px;
+        .price {
+            color: $color-primary;
+        }
+    }
+}
 </style>
