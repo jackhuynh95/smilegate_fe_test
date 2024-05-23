442693e3f492ec9e7e4b2333dfd4d6d5c49ff5bb
Jack Huynh <kietdien88@gmail.com>
2024-05-23 16:38:47 +0700

    feat: update header menu

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,18 +1,14 @@
 <script>
-import AppNavbarTop from './AppNavbarTop.vue';
 import AppNavbarBottom from './AppNavbarBottom.vue';
 export default {
     name: 'HeaderApp',
     components: {
-        AppNavbarTop,
         AppNavbarBottom,
     }
 }
 </script>
 <template>
     <header>
-        <AppNavbarTop></AppNavbarTop>
-        <hr>
         <AppNavbarBottom></AppNavbarBottom>
     </header>
 </template>
