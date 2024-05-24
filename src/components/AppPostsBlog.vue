24059c4977b9e71148ecacce89d1b8e260c76cf0
Jack Huynh <kietdien88@gmail.com>
2024-05-24 09:19:25 +0700

    feat: Utilities SCSS Responsive Design

--- src/components/AppPostsBlog.vue
+++ src/components/AppPostsBlog.vue
@@ -12,7 +12,7 @@ export default {
 <template>
     <section id="from-blog">
         <div class="container">
-            <div class="card-group d-grid grid-cols-3 gap-4">
+            <div class="card-group d-grid grid-cols-1 md:grid-cols-3 xl:grid-cols-4 two-xl:grid-cols-5 gap-4">
                 <AppPostBlog v-for="(item, idx) in Array(15).fill(1)" :key="idx" />
             </div>
         </div>
